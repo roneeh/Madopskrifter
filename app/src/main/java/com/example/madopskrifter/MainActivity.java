d93c3f4254e6836f962bd8be96cdfc8362c63548
roneeh <ronny_henriksen@live.dk>
2019-03-21 11:03:57 +0100

    Fragments and shite

--- app/src/main/java/com/example/madopskrifter/MainActivity.java
+++ app/src/main/java/com/example/madopskrifter/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.madopskrifter;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+ //       requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+
+
+        fragmentManager = getSupportFragmentManager();
+
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Forside forside = new Forside();
+        fragmentTransaction.add(R.id.mainContent,forside, null);
+        fragmentTransaction.commit();
+
+
     }
 }
