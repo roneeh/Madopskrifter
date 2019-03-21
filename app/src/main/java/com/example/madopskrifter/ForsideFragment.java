2e1c24fbcdb284ea822aaf8174c0e70a6831061e
roneeh <ronny_henriksen@live.dk>
2019-03-21 12:13:29 +0100

    lul

--- app/src/main/java/com/example/madopskrifter/ForsideFragment.java
+++ app/src/main/java/com/example/madopskrifter/ForsideFragment.java
@@ -6,6 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.concurrent.ExecutionException;
+
+import javax.xml.transform.Result;
 
 
 /**
@@ -24,6 +35,25 @@ public class ForsideFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_forside, container, false);
+
+        TextView textView = view.findViewById(R.id.SomeText);
+        try {
+            Helpers helpers = new Helpers();
+            ResultSet resultSet = helpers.execute("SELECT * FROM Brugere").get();
+            while (resultSet.next()) {
+                textView.append("Username:" + resultSet.getString("Brugernavn"));
+            }
+        }
+        catch (SQLException ex)
+        {
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
         return view;
+
+
     }
 }
